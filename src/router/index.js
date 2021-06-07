e86839b4df9f2fb37974fae6d18409e85b4ae09b
Administrator <Administrator@WIN-N0IOTJA82ES>
2021-06-07 16:18:11 +0800

    添加项目文件。

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/Home',
+    redirect: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = createRouter({
+  routes,
+  history: createWebHistory(),
+ // history:'history'
+})
+
+export default router
